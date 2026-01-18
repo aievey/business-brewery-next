9c60d1be21e469671d056aa45e81a7ff2e4c99c1
aievey <amitabh283verma@gmail.com>
2026-01-18 14:13:27 +0530

    added meta and OG

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,23 @@ import Footer from "@/components/footer";
 export const metadata = {
   title: "Business brewery",
   description:
-    "update/ renew/ or even create from scratch your business profile online.",
+    "Update/ Renew/ or even create from Scratch your business profile online with us!",
+  openGraph: {
+    title: "My Website Title",
+    description: "My website description",
+    url: "https://business-brewery-next.vercel.app/",
+    siteName: "My Website",
+    images: [
+      {
+        url: "https://business-brewery-next.vercel.app/business-brewery-cover-main.avif", // Best to point to an actual image file
+        width: 1200,
+        height: 630,
+        alt: "Business Brewery OG",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
