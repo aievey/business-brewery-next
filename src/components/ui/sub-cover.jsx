5e382421915328ec5a458474bae069d3646e7654
aievey <amitabh283verma@gmail.com>
2026-01-20 17:14:48 +0530

    Image size made dynamic

--- src/components/ui/sub-cover.jsx
+++ src/components/ui/sub-cover.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const SubCover = ({ url, children }) => {
   return (
     <div className="relative flex h-60 w-full items-center justify-center overflow-hidden rounded-2xl">
-      <Image src={url} fill alt="ok" className="-z-10 object-cover" />
+      <Image src={url} fill alt="ok" priority className="-z-10 object-cover" />
       <div className="rounded-2xl bg-stone-800/40 p-4 backdrop-blur-xs">
         <h2 className="text-4xl font-extrabold tracking-widest text-stone-50 uppercase">
           {children}
